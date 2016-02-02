90d2bde5d1be86503f7f313335c0b0509dee9ee0
luiz felipe <luizfelipe_p@hotmail.com>
2016-02-02 10:35:06 -0200

    versão v.1.2 da classe A

--- classA.java
+++ classA.java
@@ -2,6 +2,14 @@ public class classA {
 	int A1;
 	float A2;
 	string A3;
+	String Aramo4;
+
+	public String getAramo4() {
+		return Aramo4;
+	}
+	public void setAramo4(string aramo) {
+		Aramo4 = aramo;
+	}
 	public string getA1() {
 		return A3;
 	}
