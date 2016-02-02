da8caf50f30059dbb9bca28d92ed7a166228bfbe
luiz felipe <luizfelipe_p@hotmail.com>
2016-02-02 13:04:41 -0200

    classe a versão v1.2 do tronco principal

--- classA.java
+++ classA.java
@@ -2,12 +2,22 @@ public class classA {
 	int A1;
 	float A2;
 	string A3;
+	String Aprinc4
+	
+	public string getAprinc4() {
+		return Aprinc4;
+	}
+	public void setApring4(string aprinc4) {
+		Aprinc4 = aprinc4;
+	}
+	
 	public string getA1() {
 		return A3;
 	}
 	public void setA1(string a3) {
 		A3 = a3;
 	}
+	
 	public int getA1() {
 		return A1;
 	}
