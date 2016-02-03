2b05499ba2c5d2d00839f6b2ede24bc1e8e845e9
luiz felipe <luizfelipe_p@hotmail.com>
2016-02-02 23:39:34 -0200

    versão v.1.3 classeB

---
+++ classB.java
@@ -0,0 +1,36 @@
+public class classA {
+	int A1;
+	float A2;
+	string A3;
+	String Aprinc4
+	
+	public string getAprinc4() {
+		return Aprinc4;
+	}
+	public void setApring4(string aprinc4) {
+		Aprinc4 = aprinc4;
+	}
+	
+	public string getA1() {
+		return A3;
+	}
+	public void setA1(string a3) {
+		A3 = a3;
+	}
+	
+	public int getA1() {
+		return A1;
+	}
+	public void setA1(int a1) {
+		A1 = a1;
+	}
+	public float getA2() {
+		return A2;
+	}
+	public void setA2(float a2) {
+		A2 = a2;
+	}
+		public float soma(int, float){
+		return this.A1+this.A2;
+	}
+}
