0b028a53798a7ccf6ac40f8511474dee182d6870
luiz felipe <luizfelipe_p@hotmail.com>
2016-02-03 00:44:42 -0200

    addicinada classe B

---
+++ classB.java
@@ -0,0 +1,36 @@
+public class classB {
+	int A1;
+	float A2;
+	String B4;
+
+	
+	public string getB4() {
+		return B4;
+	}
+	public void setB4(string b4) {
+		B4= b4;
+	}
+	
+	public string getA1() {
+		return A3;
+	}
+	public void setA1(string a3) {
+		A3 = a3;
+	}
+	
+	public int getA1() {
+		return A1;
+	}
+	public void setA1(int a1) {
+		A1 = a1;
+	}
+	public float getA2() {
+		return A2;
+	}
+	public void setA2(float a2) {
+		A2 = a2;
+	}
+		public float soma(int, float){
+		return this.A1+this.A2;
+	}
+}
